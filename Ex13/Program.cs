cbd12ee3b881b8a68d18a68a4609a3c69cc79852
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 10:28:30 +0300

    Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num>999 || num<-999)
+{num=num/10;}
+if (num > 99)
+{
+    int num3 = (num % 10);
+    Console.WriteLine("Третья цифра числа: "+ num3);
+}
+else 
+{
+    if (num <-99)
+    {
+    int num3 = (-num % 10);
+    Console.WriteLine("Третья цифра числа: "+ num3);
+    }
+    else Console.WriteLine("Третьей цифры нет!");
+}
