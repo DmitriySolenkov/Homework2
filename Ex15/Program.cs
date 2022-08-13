5b92a5ffb535d12de82c49bd81299254c878481b
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 10:34:22 +0300

    Ex15

---
+++ Ex15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>=1 && num <=7)
+{
+    if (num ==6 || num ==7) Console.WriteLine("Выбранный день недели является выходным");
+    else Console.WriteLine("Выбранный день недели является будним");
+}
+else Console.WriteLine("Вы ввели неверное число");
