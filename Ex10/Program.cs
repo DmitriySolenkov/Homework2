dabc9fdfb4f341ecd9ae7da4b23e7fb7b15de24e
DmitriySolenkov <soledimk@gmail.com>
2022-08-13 10:05:57 +0300

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Веедите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+    int num1 = (num % 100) /10;
+    Console.WriteLine("Вторая цифра числа: "+ num1);
+}
+else
+{
+    if (num > -1000 && num < -99)
+    {
+        int num1 = (-num % 100) /10;
+    Console.WriteLine("Вторая цифра числа: "+ num1);
+    }
+    else Console.WriteLine("Число не трехзначное!");
+}
